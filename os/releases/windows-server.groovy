3de94e0c95c3e08f78ff047ba0b368155a667a40
Matthew Verive <immewnity@users.noreply.github.com>
2023-03-17 14:10:53 -0500

    Update windows-server.groovy

--- os/releases/windows-server.groovy
+++ os/releases/windows-server.groovy
@@ -18,7 +18,7 @@ if(asset.getAssetType()!=Asset.AssetType.HOST) return false;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("CurrentBuild = 3790")) return true;
+if ((win_prod_type.contains("ProductType = ServerNT") || win_prod_type.contains("ProductType = LanmanNT")) && win_prod_type.contains("CurrentBuild = 3790")) return true;
 
 if(asset.hasAnyVuln([105632,105339,105340,105658,91129,90238,90237,90388,74188,90326,105066,105079,105067,105068,105069,105078,105070,105071,105072,105076,105073,105075,105074])) return true;
 
@@ -30,7 +30,7 @@ if(asset.getAssetType()!=Asset.AssetType.HOST) return false;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("CurrentBuild = 6003")) return true;
+if ((win_prod_type.contains("ProductType = ServerNT") || win_prod_type.contains("ProductType = LanmanNT")) && win_prod_type.contains("CurrentBuild = 6003")) return true;
 
 if(asset.hasAnyVuln([105858,105449,90502,45343,90580,45341])) return true;
 
@@ -42,7 +42,7 @@ if(asset.getAssetType()!=Asset.AssetType.HOST) return false;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && (win_prod_type.contains("CurrentBuild = 7600") || win_prod_type.contains("CurrentBuild = 7601"))) return true;
+if ((win_prod_type.contains("ProductType = ServerNT") || win_prod_type.contains("ProductType = LanmanNT")) && (win_prod_type.contains("CurrentBuild = 7600") || win_prod_type.contains("CurrentBuild = 7601"))) return true;
 
 if(asset.hasAnyVuln([105859,105497,90742,45345])) return true;
 
@@ -52,11 +52,11 @@ if(asset.hasAnyVuln([105859,105497,90742,45345])) return true;
 
 if(asset.getAssetType()!=Asset.AssetType.HOST) return false;
 
+if(asset.hasAnyVuln([45350,45351])) return true;
+
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("CurrentBuild = 9200")) return true;
-
-if(asset.hasAnyVuln([45350,45351])) return true;
+if ((win_prod_type.contains("ProductType = ServerNT") || win_prod_type.contains("ProductType = LanmanNT")) && win_prod_type.contains("CurrentBuild = 9200")) return true;
 
 
 
@@ -68,8 +68,8 @@ if(asset.hasAnyVuln([45348])) return true;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("ProductName = Windows Server 2012 R2")) return true;
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("CurrentBuild = 9600")) return true;
+if (win_prod_type.contains("ProductName = Windows Server 2012 R2")) return true;
+if ((win_prod_type.contains("ProductType = ServerNT") || win_prod_type.contains("ProductType = LanmanNT")) && win_prod_type.contains("CurrentBuild = 9600")) return true;
 
 
 
@@ -81,8 +81,7 @@ if(asset.hasAnyVuln([45347,45349])) return true;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("ProductName = Windows Server 2016")) return true;
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("CurrentBuild = 14393")) return true;
+if (win_prod_type.contains("ProductName = Windows Server 2016")) return true;
 
 
 
@@ -94,7 +93,7 @@ if(asset.hasAnyVuln([45419])) return true;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("ProductName = Windows Server 2019")) return true;
+if (win_prod_type.contains("ProductName = Windows Server 2019")) return true;
 
 
 
@@ -104,4 +103,4 @@ if(asset.getAssetType()!=Asset.AssetType.HOST) return false;
 
 // Windows Product Type
 win_prod_type = asset.resultsForQid(90107L);
-if (win_prod_type.contains("ProductType = ServerNT") && win_prod_type.contains("ProductName = Windows Server 2022")) return true;
+if (win_prod_type.contains("ProductName = Windows Server 2022")) return true;
